8256d88f781a515853830307d0267eef5bec1c91
Ednilson Costa <68651715+edcostabh@users.noreply.github.com>
2021-04-22 20:15:12 -0300

    Update Solução.kt
    Update file

--- Kotlin/1 - Introdução a programação em Kotlin (Desafios)/1 - Soma Simples/Solução.kt
+++ Kotlin/1 - Introdução a programação em Kotlin (Desafios)/1 - Soma Simples/Solução.kt
@@ -3,6 +3,6 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)  // faz a leitura da linha
     val a = input.nextInt() // leitura e atribuição do primeiro número à variável "a"
-    val b = input.nextInt() // leitura e atribuição do primeiro número à variável "a"
+    val b = input.nextInt() // leitura e atribuição do segundo número à variável "b"
     println("SOMA = "+ ( a+b )) // imprime a string "SOMA = " concatenada com a soma das variávies "a" e "b"
 }
